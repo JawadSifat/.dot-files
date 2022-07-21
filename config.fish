368aba6f09c765ec2a87fe1a54d504d93b227896
Jawad Sifat <53837369+JawadSifat@users.noreply.github.com>
2022-07-21 15:23:16 +0200

    s

---
+++ config.fish
@@ -0,0 +1,29 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+### BASH REWRITTEN IN RUST
+starship init fish | source
+zoxide init fish | source
+alias cd="z"
+alias top="ytop"
+alias grep="rg"
+alias cat="bat"
+alias find="fd"
+alias ps="procs"
+alias ls="exa"
+
+
+### Aliases 
+alias l='exa --icons --long --no-user --no-time'
+alias c='clear'
+
+
+
+
+
+
+
+
+
