ec1461d38990936190f85c1f85cc2006f130d4f5
Jawad Sifat <53837369+JawadSifat@users.noreply.github.com>
2022-08-24 00:46:37 +0000

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,29 +1,41 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+## Set values
+# Hide welcome message
+set fish_greeting
+
+## Useful aliases
+# Replace ls with exa
+alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
+alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first --icons'  # long format
+alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="exa -a | egrep '^\.'"                                     # show only dotfiles
+alias ip="ip -color"
+
+# Replace some more things with better alternatives
+alias cat='bat --style header --style rules --style snip --style changes --style header'
+
+# Common use
+alias grubup="sudo update-grub"
+alias fixpacman="sudo rm /var/lib/pacman/db.lck"
+alias tarnow='tar -acf '
+alias untar='tar -xvf '
+alias wget='wget -c '
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias hw='hwinfo --short'                          # Hardware Info
+
+
+## Run fastfetch if session is interactive
+if status --is-interactive && type -q fastfetch
+   fastfetch --load-config neofetch
 end
-
-
-### BASH REWRITTEN IN RUST
-starship init fish | source
-zoxide init fish | source
-alias cd="z"
-alias top="ytop"
-alias grep="rg"
-alias cat="bat"
-alias find="fd"
-alias ps="procs"
-alias ls="exa"
-
-
-### Aliases 
-alias l='exa --icons --long --no-user --no-time'
-alias c='clear'
-
-
-
-
-
-
-
-
-
